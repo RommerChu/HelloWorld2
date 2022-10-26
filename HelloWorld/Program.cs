064a663ca89dc282feb5798f78f6e6f9c6ded842
Rommer Chu <rommer.chu@gmail.com>
2022-10-26 11:38:03 -0400

    add line 3

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("line 2");
+Console.WriteLine("Line 3");
 Console.ReadLine();
 
