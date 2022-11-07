137aab8b92eae34c721260321b7324efa06386b1
Rommer Chu <rommer.chu@gmail.com>
2022-11-07 15:16:40 -0500

    added lines
    added three lines

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,5 +6,8 @@ Console.WriteLine("Line 4");
 Console.WriteLine("Line 5");
 Console.WriteLine("Line 6");
 Console.WriteLine("Line 7");
+Console.WriteLine("Line 8");
+Console.WriteLine("Line 9");
+Console.WriteLine("Line 10");
 Console.ReadLine();
 
